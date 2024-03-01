3edd9e4148f4e7a2a4d11310400592a8901aafd1
Nicholas Eisenberg <DEVNET\eisenbergn@stl-b324166.dev.net>
2024-03-01 12:46:55 -0800

    fixed the path appending in method2

--- method1/experiment/__main__.py
+++ method1/experiment/__main__.py
@@ -1,5 +1,3 @@
-from sys import path
-path.append("/home/local/DEVNET/eisenbergn/Tmp/relimports/method1")
 from src.run_exp import run_synth_100m_exp
 
 if __name__ == "__main__":
--- method2/experiments/exp1/__main__.py
+++ method2/experiments/exp1/__main__.py
@@ -1,5 +1,11 @@
+import os
+root = __file__
+root = root.split(os.sep)
+root = os.path.join(os.sep, os.path.join(*root[:-3]))
+print(root)
 from sys import path
-path.append("/home/local/DEVNET/eisenbergn/Tmp/relimports/method2")
+path.append(root)
+
 from src.run_exp import run_synth_100m_exp
 
 if __name__ == "__main__":
